05d9c9f55481b9edfd53b01b718edd1e018b032a
Ricky Su <ricky@ez2.us>
2015-11-10 19:03:00 +0800

    update travis build script

--- .travis.scripts/compile.sh
+++ .travis.scripts/compile.sh
@@ -9,6 +9,12 @@ cd "$thirdparty/r3" && \
 ./autogen.sh && \
 CFLAGS="-O2 -fPIC" ./configure --prefix="$thirdparty/build" && make clean install
 
+cd "$thirdparty/http-parser" && \
+PREFIX="$thirdparty/build" make clean && \
+PREFIX="$thirdparty/build" make package && \
+PREFIX="$thirdparty/build" make install && \
+cp libhttp_parser.o "$thirdparty/build/lib"
+
 cd $currentpwd
 
 phpize
